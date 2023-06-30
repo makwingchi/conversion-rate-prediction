da89d14de91935d1a2753dc6ec70dfa8c01430af
“rongzhimai” <makwin9chi@gmail.com>
2023-06-30 16:14:31 +0800

    update configuration structure

--- PaddleDynamic/config.py
+++ PaddleDynamic/config.py
@@ -1,63 +1,50 @@
 def get_configurations():
-    return {
+    config = {
         "runner": {
             "seed": 666,
             "device": "gpu",
-            "model_save_path": "./output_dnn",
-            "train_data_path": "../data/data205411/2023-cvr-contest-data/train_data",
-            "test_data_path": "../data/data204194/test_data",
+            "model_save_path": "./output_t1",
+            "train_data_path": "../work_data/data205411/2023-cvr-contest-data/train_t1",
+            "test_data_path": "../work_data/data204194/test_t1",
             "batch_size": 512,
             "train_epochs": 1,
-            "infer_start_epoch": 0,
-            "infer_end_epoch": 1,
             "thread_num": 1,
             "print_interval": 50,
-            "max_len": 21,
-            "model_type": "baseline",
+            "max_len": 20,
+            "model_type": "naive_attention",
             "task_type": "single",
-            "conv_type": "1"
+            "conv_type": "1",
+            "is_infer": False
         },
         "optimizer": {
             "learning_rate": 0.001
         },
         "models": {
-            "baseline": {
-                "sparse_feature_dim": 9,
-                "sparse_feature_number": 88000000,
-                "num_fields": 26,
-                "fc_sizes": [512, 256, 128, 32]
-            },
-            "deepcrossing": {
-                "sparse_feature_dim": 9,
-                "sparse_feature_number": 88000000,
-                "num_fields": 26,
-                "fc_sizes": [512, 256, 128, 32]
-            },
-            "wideanddeep": {
-                "sparse_feature_dim": 9,
+            "common": {
+                "sparse_feature_dim": 12,
                 "sparse_feature_number": 88000000,
                 "num_fields": 26,
                 "fc_sizes": [512, 256, 128, 32]
             },
             "deepandcross": {
-                "sparse_feature_dim": 9,
-                "sparse_feature_number": 88000000,
-                "num_fields": 26,
-                "fc_sizes": [512, 256, 128, 32],
                 "num_crosses": 3
             },
-            "fnn": {
-                "sparse_feature_dim": 9,
-                "sparse_feature_number": 88000000,
-                "num_fields": 26,
-                "fc_sizes": [512, 256, 128, 32]
-            },
             "pnn": {
-                "sparse_feature_dim": 9,
-                "sparse_feature_number": 88000000,
-                "num_fields": 26,
-                "fc_sizes": [512, 256, 128, 32],
                 "num_matrices": 8
+            },
+            "mmoe": {
+                "expert_num": 3,
+                "expert_size": 256,
+                "tower_size": 128,
+                "gate_num": 3
+            },
+            "native_attention": {
+                "qkv_dim": 9
             }
         }
     }
+
+    config["runner"]["infer_start_epoch"] = config["runner"]["train_epochs"] - 1
+    config["runner"]["infer_end_epoch"] = config["runner"]["train_epochs"]
+
+    return config
